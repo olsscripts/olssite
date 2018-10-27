26b4ed894ee1e7a65f789d899cc1e88c5f853caa
xpressos <chrisdavies2000@gmail.com>
2018-10-27 01:31:13 +0100

    Update olssite.sh

--- olssite.sh
+++ olssite.sh
@@ -848,7 +848,7 @@ while [ "$1" != "" ] ; do
                                     ADMINPASSWORD=$FOLLOWPARAM
                                     ;;
 									
-			 --adminport )          check_value_follow "$2" "Admin port"
+             --adminport )          check_value_follow "$2" "Admin port"
                                     shift
                                     ADMINPORT=$FOLLOWPARAM
                                     ;;						
@@ -975,7 +975,7 @@ if [ "x$INSTALLSITE" = "x1" ] ; then
     echoY "Site HTTP port:      " "$SITEPORT"
     echoY "Site HTTPS port:     " "$SSLSITEPORT"
     echoY "Site domain:         " "$SITEDOMAIN"
-    echoY "Site location:       " "$SITEPATH"
+    #echoY "Site location:       " "$SITEPATH"
   
     #if [ -e "$SITEPATH" ] ; then
         #echoY "Site location:       " "$SITEPATH (Existing)"
